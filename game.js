e973184c778d87c3d2e517bbaecee7f0e58d52b0
Tej Raghuveer <tejmotipalli@gmail.com>
2026-03-14 14:55:10 +0530

    update checkpoint 2 files

---
+++ game.js
@@ -0,0 +1,8 @@
+console.log("TapTap Game Engine Started!");
+
+document.body.style.background = "black";
+document.body.style.color = "white";
+
+const text = document.createElement("h2");
+text.innerText = "Game Engine Running 🚀";
+document.body.appendChild(text);
